dd29075ab6b229d94ecfd4020b82ba95c3e93b4d
Zachary Carter <thezachcarter1449@gmail.com>
2025-01-28 18:37:42 -0700

    Added LOAD instruction functionality

--- src/UVSim/functions/LOAD.js
+++ src/UVSim/functions/LOAD.js
@@ -1,10 +1,12 @@
 /**
- *
+ * Load a word from a specific location in memory into the accumulator.
  * @param {current virtual machine - has all the data like registers memory and such} vm
  * @param {value - the value passed by the instruction otherwise known as operand} v
  */
 const LOAD = (vm, v) => {
   console.log("LOAD");
+
+  vm.r.acc = vm.memory.getLoc(v);
 };
 
 module.exports = { LOAD };
