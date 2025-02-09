4e65c2b20bd45976bcd10c8ad9fd0b7b7aded168
ZachAsmus <84919168+ZachAsmus@users.noreply.github.com>
2025-02-08 18:26:25 -0700

    Update MULTIPLY.js
    Added console.log message for testing purposes

--- src/UVSim/functions/MULTIPLY.js
+++ src/UVSim/functions/MULTIPLY.js
@@ -6,6 +6,7 @@
  * value - the value passed by the instruction otherwise known as operand
  */
 const MULTIPLY = (vm, v) => {
+  console.log("MULTIPLY");
   // multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
   vm.r.acc *= vm.memory.getLoc(v);
 }
