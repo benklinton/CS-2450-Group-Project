3dffbda7f58c3e1a949eb02d3a47b4da61d3e914
ZachAsmus <84919168+ZachAsmus@users.noreply.github.com>
2025-03-22 21:30:54 -0600

    Fixed Home.jsx

--- src/renderer/src/Pages/Home.jsx
+++ src/renderer/src/Pages/Home.jsx
@@ -5,9 +5,9 @@ import { ColorSelector } from "../Components/Colors";
 
 export const HomePage = () => {
   return (
-    <div className="container home-container pt-3">
+    <div className="container home-container bg-light pt-3">
       <div className="row mh-100 g-3">
-        <div className="col-lg-8c h-100 mh-100">
+        <div className="col-lg-8 h-100 mh-100">
           <Console />
         </div>
         <div className="col-lg-4 d-flex flex-column gap-3 max-height">
