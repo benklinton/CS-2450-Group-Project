e9efdb0f1ec9c0d17b08bec56c0f38e3b8306497
Ben Linton <benklinton@gmail.com>
2025-04-12 21:24:31 -0600

    updated README and deleted old test system

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -4,18 +4,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import { GlobalContextProvider } from "./Context/GlobalContext";
 import { HomePage } from "./Pages/Home";
 import { MessagingContextProvider } from "./Context/MessagingContext";
-import { TestingContextProvider } from "./Context/TestingContext";
 import { ColorContextProvider } from "./Context/ColorContext";
 
 function App() {
   return (
     <GlobalContextProvider>
       <MessagingContextProvider>
-        <TestingContextProvider>
           <ColorContextProvider>
             <HomePage />
           </ColorContextProvider>
-        </TestingContextProvider>
       </MessagingContextProvider>
     </GlobalContextProvider>
   );
